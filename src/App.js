3b236451a4f5fd37f3540f216bd4c6c25781cf38
matte-134 <matthew.eland@nandos.co.uk>
2022-10-04 12:03:28 +0100

    pokemon images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
 
@@ -6,13 +6,22 @@ const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([])
+
+  useEffect(() => {
+    fetch(pokeApi).then((res) => res.json()).then((data) => setPokemonList(data.results))
+  });
+
+  console.log(pokemonList);
 
   return (
     <div data-testid="app">
       <Navigation />
 
       <h1>Pokemon should appear here</h1>
+      {pokemonList.map((pokemon) => <PokemonCard url={pokemon.url} name={pokemon.name}/>)}
       <PokemonCard />
+      
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card'
 
 function PokemonCard({ url, name }) {
+  const [pokemon, setPokemon] = useState(null)
+
+  useEffect(() => {
+    fetch(url).then((res) => res.json()).then((data) => setPokemon(data))
+  })
+
 
   return (
     <div>
-        pokemon card
+        <Card style={{ width: '18rem' }}>
+          <Card.Img src={pokemon?.sprites.front_default}>
+
+          </Card.Img>
+          <Card.Body>
+            <Card.Title>
+              {name}
+            </Card.Title>
+            <Card.Text>
+              <ul>
+              {pokemon?.abilities.map((ability) => 
+              <li>{ability.ability.name}</li>)}
+              </ul>
+            </Card.Text>
+          </Card.Body>
+          </Card>
     </div>
   );
 }
