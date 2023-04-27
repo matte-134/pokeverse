fb4ac5c99a56e8f205d83eb13531193d601ccffe
VicBriar <spartanginger@gmail.com>
2023-04-26 20:24:32 -0700

    feat: pokemon app renders card with sprites

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
+import Container from 'react-bootstrap/esm/Container';
+import { useEffect } from 'react';
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
+
+
 function App() {
+  
+  const [data, setData] = useState([]);
+
+  async function fetchPokemon() {
+    try {
+
+      const request = await fetch(pokeApi);
+      const result = await request.json();
+      setData(await result.results);
+
+    } catch(error) {
+      throw new Error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchPokemon();
+  },[])
 
   return (
     <div data-testid="app">
       <Navigation />
 
-      <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      <h1>Cards should appear here</h1>
+      <Container>
+        {data.map((pokemon,indx) => {
+          return (<PokemonCard key={indx} pokemon={pokemon} />);
+        })}
+      </Container>
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {Card} from 'react-bootstrap';
 
-function PokemonCard({ url, name }) {
+function PokemonCard({pokemon, getPokemon}) {
+  const defaultPokeData = {data:{sprites:{front_default:""}},loading: true};
+  const [pokeData, setPokeData] = useState(defaultPokeData);
+  const {name, url} = pokemon;
 
-  return (
-    <div>
-        pokemon card
-    </div>
-  );
-}
+  async function getPokemon(){
+    try{
+      const response = await fetch(url);
+      const data = await response.json();
+      setPokeData({data:data,loading: false});
+    }catch(error){
+      throw new Error(error);
+    }
+  }
+  
+  useEffect(() => {getPokemon()},[]);
+
+  if(pokeData.loading) {
+    return <><p>Loading...</p></>
+  }else{
+    return (
+      <Card>
+        <Card.Img variant="top" src={pokeData.data.sprites.front_default}></Card.Img>
+        <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        </Card.Body>
+      </Card>
+    );
+  }
+  }
 
 export { PokemonCard };
