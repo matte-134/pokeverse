caf385a3bf40c373cfb8c67c917d696e38e3a589
Jonathan Jacka <jonathanjacka@gmail.com>
2023-05-08 16:23:45 -0500

    Updates App and Navigation components, updates parcel version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
-import { Search } from './components/Search.js'
+import { Search } from './components/Search.js';
 import Container from 'react-bootstrap/esm/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -10,42 +10,39 @@ import { useEffect } from 'react';
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
-
-
 function App() {
-  
   const [data, setData] = useState([]);
 
   async function fetchPokemon() {
     try {
-
       const request = await fetch(pokeApi);
       const result = await request.json();
-      setData(await result.results);
-
-    } catch(error) {
-      throw new Error(error)
+      setData(result.results); // await resolves a Promise, so since you're not doing this here, no need to use await
+    } catch (error) {
+      // You don't need to create a new error here as it would be redundant
+      // If we hit the catch block, an error has already been thrown, and you just need to handle it
+      console.error(error.message);
     }
   }
 
   useEffect(() => {
     fetchPokemon();
-  },[])
+  }, []);
 
   return (
-    <div data-testid="app">
+    <div data-testid='app'>
       <Navigation />
       <Search />
 
       <Container>
-        <Row xs={2} md={6} lg={10} >
-        {data.map((pokemon,indx) => {
-          return (
-          <Col key={indx}>
-          <PokemonCard key={indx} pokemon={pokemon} />
-          </Col>
-          )
-        })}
+        <Row xs={2} md={6} lg={10}>
+          {data.map((pokemon, indx) => {
+            return (
+              <Col key={indx}>
+                <PokemonCard key={indx} pokemon={pokemon} />
+              </Col>
+            );
+          })}
         </Row>
       </Container>
     </div>
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,55 +1,71 @@
 import React, { useEffect, useState } from 'react';
-import {Card} from 'react-bootstrap';
-import squirtle from '../../public/squirtle.png'
+import { Card } from 'react-bootstrap';
+import squirtle from '../../public/squirtle.png';
 // import loadingChimecho from '../../public/loadingChimecho.gif';
 // import cool from '../../public/cool.jpeg'
 
-function PokemonCard({pokemon, getPokemon}) {
-  const defaultPokeData = {data:{sprites:{front_default:""}},loading: true};
+function PokemonCard({ pokemon }) {
+  const defaultPokeData = {
+    // eslint-disable-next-line camelcase
+    data: { sprites: { front_default: '' } },
+    loading: true,
+  };
   const [pokeData, setPokeData] = useState(defaultPokeData);
-  const {name, url} = pokemon;
+  const { name, url } = pokemon;
 
-  async function getPokemon(){
-    try{
-      const response = await fetch(url);
-      const data = await response.json();
-      setPokeData({data:data,loading:false});
-    }catch(error){
-      throw new Error(error);
+  useEffect(() => {
+    async function getPokemon() {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setPokeData({ data: data, loading: false });
+      } catch (error) {
+        // You don't need to create a new error here as it would be redundant
+        // If we hit the catch block, an error has already been thrown, and you just need to handle it
+        console.error(error.message);
+      }
     }
-  }
-  
-  useEffect(() => {getPokemon()},[]);
+    getPokemon();
+  }, [url]);
 
-  if(pokeData.loading) {
+  if (pokeData.loading) {
     return (
       <Card className='mt-4'>
-      <Card.Img className="d-flex justify-content-center" variant="top" src={squirtle}/>
-      <Card.Body>
-        <Card.Title className='text-center'>loading pokemon..</Card.Title>
-          <Card.Text as="div">
-              <ul>Thinking</ul>
-              <ul>Fetch</ul>
-          </Card.Text>
-      </Card.Body>
-    </Card>
-    );
-   } else{
-    return (
-      <Card className='mt-4'>
-        <Card.Img className="d-flex justify-content-center" variant="top" src={pokeData.data.sprites.front_default}></Card.Img>
+        <Card.Img
+          className='d-flex justify-content-center'
+          variant='top'
+          src={squirtle}
+        />
         <Card.Body>
-          <Card.Title className='text-center'>{name}</Card.Title>
-            <Card.Text as="div">
-                {pokeData.data.abilities.map((ability,index) => {
-                  console.log(ability.ability.name)
-                  return <ul key={index}>{ability.ability.name}</ul>
-              })}
-            </Card.Text>
+          <Card.Title className='text-center'>loading pokemon..</Card.Title>
+          <Card.Text as='div'>
+            <ul>Thinking</ul>
+            <ul>Fetch</ul>
+          </Card.Text>
         </Card.Body>
       </Card>
     );
   }
-  }
+
+  return (
+    <Card className='mt-4'>
+      <Card.Img
+        className='d-flex justify-content-center'
+        variant='top'
+        src={pokeData.data.sprites.front_default}
+      ></Card.Img>
+      <Card.Body>
+        <Card.Title className='text-center'>{name}</Card.Title>
+        <Card.Text as='div'>
+          <ul>
+            {pokeData.data.abilities.map((ability, index) => {
+              return <li key={index}>{ability.ability.name}</li>;
+            })}
+          </ul>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
 
 export { PokemonCard };
